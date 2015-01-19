35ea0259ca904661bc48de8e6d31a8476983a9f7
anniebae <bae.annie@gmail.com>
2015-01-18 22:12:41 -0500

    todo model created

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,1 +1,13 @@
-todo.js
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+  defaults: {
+    title: '',
+    completed: false
+  },
+  toggle: function() {
+    this.save({
+      completed: !this.get('completed')
+    });
+  }
+});
